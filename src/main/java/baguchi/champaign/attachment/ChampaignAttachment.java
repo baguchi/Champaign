625604c27840ab57ab08cff6c433288e27151e79
baguchan <shutux500@gmail.com>
2025-02-16 18:54:41 +0900

    1.21.4-1.1.0

--- src/main/java/baguchi/champaign/attachment/ChampaignAttachment.java
+++ src/main/java/baguchi/champaign/attachment/ChampaignAttachment.java
@@ -21,6 +21,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.player.Player;
@@ -132,6 +133,10 @@ public class ChampaignAttachment implements INBTSerializable<CompoundTag> {
                 if (entity instanceof Mob mob) {
                     mob.finalizeSpawn(serverLevel, serverLevel.getCurrentDifficultyAt(player.blockPosition()), EntitySpawnReason.MOB_SUMMONED, null);
                     mob.dropPreservedEquipment(serverLevel);
+                }
+
+                if (entity instanceof TamableAnimal mob) {
+                    mob.tame(player);
                 }
                 if (!player.isCreative()) {
                     player.getInventory().clearOrCountMatchingItems(predicate -> {
